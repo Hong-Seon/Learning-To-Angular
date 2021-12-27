bf0c7b121b51ca079fd52fced4fface843d1295b
Hong-Seon <ahs9702@daum.net>
2021-12-27 15:51:39 +0900

    add and sort article

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 10),
+      new Article('Fullstack', 'http://angular.io', 12),
+      new Article('Spring', 'http://angular.io', 14)
+    ]
+  }
   addArticle(title: HTMLInputElement, link:HTMLInputElement): boolean {
     console.log(`Adding article" title : ${title.value} and link : ${link.value}`);
+
+    this.articles.push(
+      new Article(title.value, link.value, 0)
+      )
+      title.value='';
+      link.value='';
     return false
   }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b:Article) => b.votes - a.votes);
+  }
+  
   title = 'angular-reddit';
 }
